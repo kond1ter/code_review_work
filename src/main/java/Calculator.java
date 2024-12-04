5d6a9289c85c88dadc8894d3728c4b23ac68fd51
qondeeter <61204648+kond1ter@users.noreply.github.com>
2024-12-05 00:58:07 +0300

    Update Calculator.java

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -2,7 +2,7 @@ public class Calculator {
     public int add(int a, int b){
         return a + b;
     }
-    public int dif(int a, int b){
+    public int diff(int a, int b){
         return a - b;
     }
     public int div(int a, int b){
